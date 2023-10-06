6c2b54baba4ca21d06c913c9f3712049419560ed
Sarah <sarahwambui843@gmail.com>
2023-10-06 23:30:56 +0300

    :sparkles: feat: Introduction to Python

---
+++ 01-Day-Introduction/hello.py
@@ -0,0 +1,25 @@
+# Introduction
+# Day 1-30DaysOfPython Challenge
+
+
+print("hello wolrd!")
+
+# Mathematical operations
+print(2+3)       # addition(+)
+print(3 - 1)     # subtraction(-)
+print(2 * 3)     # multiplication(-)
+print(3 / 2)     # division(/)
+print(3 ** 2)    # exponential(**)
+print(3 % 2)     # modulous(%)
+print(3 // 2)    # Floor division operator(//)
+
+# Checking data types
+
+print(type(10))                   # Int
+print(type(3.14))                 # Float
+print(type(1 + 3j))               # Complex
+print(type("Sarah"))              # String
+print(type([1, 2, 3, 4]))         # List
+print(type({"name": "Sarah"}))    # Dictionary
+print(type({9.8, 3.14, 2.7}))     # Set
+print(type((9.8, 3.14, 2.7)))     # Tuple       
