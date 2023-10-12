210d3a610d50e57abec8292ca0268ce3617f3987
Sarah <sarahwambui843@gmail.com>
2023-10-12 23:10:36 +0300

    added list methods

--- 05-Day-Lists/day_5.py
+++ 05-Day-Lists/day_5.py
@@ -49,3 +49,73 @@ print(fruit1)
 print(fruit2) 
 print(fruit3) 
 print(rest)  
+
+# slicing items from a List
+f = ['banana', 'orange', 'mango', 'lemon','lime','apple']
+all_fruits = f[0:6]
+all_fruit = fruit[0:]
+orange_and_mango = f[1:3]
+print(all_fruits)
+print(all_fruit)
+print(orange_and_mango)
+
+# Mofifying Lists
+many_fruits =  ['banana', 'orange', 'mango', 'lemon']
+many_fruits[0] = 'avocado'
+print(many_fruits)
+many_fruits[1] = 'apple'
+print(many_fruits)
+
+# Checking Items in a List
+fruite =  ['banana', 'orange', 'mango', 'lemon']
+does_exist = 'banana' in fruite
+print(does_exist)
+do_exist = 'lime' in fruite
+print(do_exist)
+
+# Adding Items to a List
+fruite.append('lime')
+print(fruite)
+fruite.append('apple')
+print(fruite)
+
+# Inserting Items into a List
+# We can use insert() method to insert a single item at a specified index in a list.
+# insert(index, item)
+fruite = ['banana', 'orange', 'mango', 'lemon']
+fruite.insert(2, 'apple')
+print(fruite)
+fruite.insert(3, 'lime')
+print(fruite)
+
+# Removing Items from a List
+# remove(item)
+# The remove method removes a specified item from a list
+fruite = ['banana', 'orange', 'mango', 'lemon']
+fruite.remove('banana')
+print(fruite)
+fruite.remove('lemon')
+print(fruite)
+
+# Removing Items Using pop
+# Remove the last item if not specified index
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.pop()
+print(fruits)
+# Remove the item at the specified index
+fruit.pop(0)
+print(fruit)
+
+# Removing Items using del
+#  The del keyword removes the specified index and it can also be used to delete items within index range.
+fruits = ['banana', 'orange', 'mango', 'lemon', 'kiwi', 'lime']
+del fruits[0]
+print(fruits)
+
+# Copying a List
+# It is possible to copy a list by reassigning it to a new variable in the following way: list2 = list1.
+fruits  = ['banana', 'orange', 'mango', 'lemon']
+fruits_copy = fruits.copy()
+print(fruits_copy)
+
+
